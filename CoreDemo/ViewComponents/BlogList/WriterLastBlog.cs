a5348c4abc31bf69f0de7cb93b18661628d76960
emrah <emrahyilmaz57@outlook.com.tr>
2022-03-28 21:20:54 +0000

    Yazar ve Blog arasındaki ilişki sağlandı.

---
+++ CoreDemo/ViewComponents/BlogList/WriterLastBlog.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.EntityFramework;
+using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.BlogList
+{
+    public class WriterLastBlog : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
+    }
+}
